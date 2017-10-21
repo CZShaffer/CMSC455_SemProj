0f693dcd222fb40741bb05619fa2404586ccfd96
CZShaffer <pb10459@umbc.edu>
2017-10-21 15:15:42 -0400

    Created script for filtering out unwanted parameters from tweet data

---
+++ datasets/src/cleanData.py
@@ -0,0 +1,31 @@
+import json
+# import time
+
+def formatLine(line, params):
+    newTokens = {}
+    line_object = json.loads(line)
+    for field in params:
+        if field not in line_object:
+            return {}
+        newTokens[field] = line_object[field]
+    json_str = json.dumps(newTokens)
+    return json_str
+
+def main():
+   filename = ""
+   writeFile = "cleaned.json"
+   file = open(filename)
+   outfile = open(writeFile,"w")
+   params = ["created_at", "text", "coordinates", "lang"]
+   for line in file:
+       newline = formatLine(line, params)
+       if bool(newline) is True:
+           outfile.write(newline)
+           outfile.write("\n")
+
+   file.close()
+   outfile.close()
+
+# start_time = time.time()
+main()
+# print("--- %s seconds ---" % (time.time() - start_time))
