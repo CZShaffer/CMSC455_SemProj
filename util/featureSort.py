8d260281035d725ba85af3d1c1201325769bc8de
CZShaffer <pb10459@umbc.edu>
2017-11-04 00:59:27 -0400

    Added methods to featureSort.py to allow writing to file after the sort without filtering

--- util/featureSort.py
+++ util/featureSort.py
@@ -26,12 +26,45 @@ def deepSort(items,param,deepParam):
         if line[param] is not None:
             deepJson = json.dumps(line[param])
             deepLine = json.loads(deepJson)
-            print(deepLine)
+            # print(deepLine)
             if deepLine[deepParam] not in sortedDict:
                 sortedDict[deepLine[deepParam]] = []
             sortedDict[deepLine[deepParam]].append(json.dumps(line))
 
     return sortedDict
+
+def writeSorted(filename, param, outfile):
+    file = open(filename, "r")
+    featureDict = featureSort(file, param)
+    for key,item in featureDict.items():
+        outfile.write(key)
+        for line in item:
+            outfile.write("\t")
+            outfile.write(line)
+            outfile.write("\n")
+
+def writeDeepSorted(filename, param, deepParam, outfile):
+    file = open(filename, "r")
+    featureDict = deepSort(file, param, deepParam)
+    for key,item in featureDict.items():
+        outfile.write(key)
+        for line in item:
+            outfile.write("\t")
+            outfile.write(line)
+            outfile.write("\n")
+
+def multiWrite(filename, paramVals):
+    outname = ""
+    for item in paramVals:
+        if len(item) is 1:
+            outname = str(item[0])
+            outname += ".json"
+            writeSorted(filename,item[0],open(outname,"w"))
+        if len(item) is 2:
+            outname = item[0]
+            outname += ".json"
+            writeDeepSorted(filename,item[0],item[1],open(outname,"w"))
+
 
 def filter(items,param,values):
     featureDict = featureSort(items,param)
@@ -69,13 +102,16 @@ def main():
     writeFile = "output.json"
     file = open(filename, "r")
     outfile = open(writeFile, "w")
+    testfile = open("testoutput.json", "w")
     # searchPV is the parameters and values to filter by. Expected to be of the format:
     # [(param1, [value11,value12,...,value1n]), (param2, [value21, value22,...,value2n]),...,(paramn, [valuen1, valuen2,..., valuenn])]
     # in cases where the parameter contains a json object, the tuple should be of the form:
     # (param1, deepParam1, [value11,value12,...,value1n])
     searchPV = [("lang",["en","ja"]),("place","country_code",["US", "JP"])]
+    writePV = [["lang"],["place", "country_code"]]
 
     outDict = multiFilter(file, searchPV)
+    multiWrite(filename, writePV)
 
     for item in outDict:
         outfile.write(item)
